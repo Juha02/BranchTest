2cd108f6d09d8814bf3daa28ab7132b779779289
Juha02 <88876981+Juha02@users.noreply.github.com>
2022-01-27 13:59:30 +0200

    suorakulmaisuuden tarkistus lisätty

---
+++ xfunktio.py
@@ -0,0 +1,29 @@
+# X-funktio tarkistaa onko kulma suora
+
+from pickle import FALSE, TRUE
+
+
+def suorakulma(sivuA, sivuB, lavistaja):
+    """Tarkistaa suorakulmaisuuden käyttäen Pythagoraan lausetta
+
+    Args:
+        sivuA (float): Ensimmäisen pituus
+        sivuB (float): Toisen pituus
+        lavistaja (float): Lävistäjän pituus
+
+    Returns:
+        boolean: TRUE -> suorakulma
+    """
+    A_nelio = sivuA * sivuA
+    B_nelio = sivuB * sivuB
+    l_nelio = lavistaja * lavistaja
+
+    if A_nelio + B_nelio == l_nelio:
+        suora = True
+    else:
+        suora = False
+    return suora
+
+# Testataan, että toimii, poista tämä myöhemmin
+vastaus = suorakulma(4, 4, 5)
+print (vastaus)
